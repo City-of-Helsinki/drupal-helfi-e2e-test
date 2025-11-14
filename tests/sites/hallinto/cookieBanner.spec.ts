c0d30e81c76f7641d94c80fe420ae121cb970048
Tero Elonen <tero.elonen@druid.fi>
2025-11-14 18:40:46 +0200

    Update cookie banner tests on hallinto instance to check iframes before and after accepting the cookies

--- tests/sites/hallinto/cookieBanner.spec.ts
+++ tests/sites/hallinto/cookieBanner.spec.ts
@@ -6,9 +6,37 @@ test.beforeEach(async ({ context }) => {
   logger('Cleared all cookies before test');
 });
 
+// Helper functions
+async function navigateToPageWithVideos(page: Page) {
+  await page.goto('/fi/paatoksenteko-ja-hallinto/helfin-sisallontuottajan-opas/sivujen-rakentaminen-drupalissa/komponentit/videoupotus', { waitUntil: 'domcontentloaded' });
+  await page.waitForSelector('.hds-cc__container');
+  await expect(page.locator('.hds-cc__container')).toBeVisible();
+}
+
 test.describe('Cookie Banner', () => {
-  test('should be visible on initial page load', async ({ page }) => {
-    await page.goto('/fi/paatoksenteko-ja-hallinto/helfin-sisallontuottajan-opas/sivujen-rakentaminen-drupalissa/koulutusvideot', { waitUntil: 'domcontentloaded' });
-    await expect(page.locator('.hds-cc__container')).toBeVisible();
+  test('should hide the remote video paragraphs content from user before accepting cookies', async ({ page, context }) => {
+    await navigateToPageWithVideos(page);
+
+    await page.waitForSelector('.components--upper');
+
+    const videosOnPage = await page.locator('.component--remote-video').all();
+
+    for (const video of videosOnPage) {
+      await expect(video.locator('iframe')).toHaveCount(0);
+    }
+  });
+
+  test('should show the remote video paragraphs content after accepting all cookies', async ({ page, context }) => {
+    await navigateToPageWithVideos(page);
+    const acceptAllCookiesButton = page.locator('.hds-cc__all-cookies-button');
+    await acceptAllCookiesButton.click();
+
+    await page.waitForSelector('.components--upper');
+
+    const videosOnPage = await page.locator('.component--remote-video').all();
+
+    for (const video of videosOnPage) {
+      await expect(video.locator('iframe')).toHaveCount(1);
+    }
   });
 });
