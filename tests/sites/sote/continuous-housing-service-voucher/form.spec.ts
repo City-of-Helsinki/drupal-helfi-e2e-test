0a65b41fbdfe1de94710d3a02603dfea13ad32e5
Tero Elonen <tero.elonen@druid.fi>
2025-11-20 16:05:57 +0200

    Continuous housing service voucher: Adjust testing values a bit, add working test for the empty form submit errors

--- tests/sites/sote/continuous-housing-service-voucher/form.spec.ts
+++ tests/sites/sote/continuous-housing-service-voucher/form.spec.ts
@@ -10,15 +10,67 @@ test.beforeEach(async ({ page }) => {
 test('On submit, unfilled fields give an error', async ({ page }) => {
   await clickResultsButton(page);
 
-  expect(await page.getByLabel('Virheilmoitus').isVisible());
+  // General error message should be visible.
+  const errorMessage = page.getByLabel('Täytäthän puuttuvat tiedot.');
+  expect(errorMessage.isVisible()).toBeTruthy();
+
+  // Find all required fields and their error messages.
+  const requiredFields = await page.locator('.input--required').all();
+  const requiredFieldMessages = await page.locator('.input--required .hdbt-error-text').all();
+
+  // Required fields should be marked as invalid.
+  for (const requiredField of requiredFields) {
+    await expect(requiredField).toContainClass('hds-text-input--invalid');
+  }
+
+  // Required fields should have error messages visible.
+  for (const requiredFieldMessage of requiredFieldMessages) {
+    await expect(requiredFieldMessage).toContainText('Kenttä on pakollinen.');
+  }
+});
+
+test('Net income per month input must be positive and in range', async ({ page }) => {
+  await page.getByLabel('Hakijan nettotulot kuukaudessa (euroa)').fill('-1');
+  await page.getByRole('button', { name: 'Laske arvio' }).click();
   expect(
     await page
-      .getByRole('link', { name: 'Hakijan nettotulot kuukaudessa (euroa)' })
+      .getByText(
+        'Arvon pitää olla 0 tai enemmän: Hakijan nettotulot kuukaudessa.',
+      )
       .isVisible(),
-  );
+  ).toBeTruthy();
+
+  await page.getByLabel('Hakijan nettotulot kuukaudessa (euroa)').fill('10000€');
+  await page.getByRole('button', { name: 'Laske arvio' }).click();
+
   expect(
     await page
-      .getByRole('link', { name: 'Palveluasumisen vuorokausihinta (euroa)' })
+      .getByText(
+        'Arvon pitää olla 0 tai enemmän: Hakijan nettotulot kuukaudessa.',
+      )
       .isVisible(),
-  );
+  ).toBeFalsy();
+});
+
+test('Service provider price per 24h input must be positive and in range', async ({ page }) => {
+  await page.getByLabel('Palveluasumisen vuorokausihinta (euroa)').fill('-1');
+  await page.getByRole('button', { name: 'Laske arvio' }).click();
+  expect(
+    await page
+      .getByText(
+        'Arvon pitää olla 0 tai enemmän: Palveluasumisen vuorokausihinta.',
+      )
+      .isVisible(),
+  ).toBeTruthy();
+
+  await page.getByLabel('Palveluasumisen vuorokausihinta (euroa)').fill('100€');
+  await page.getByRole('button', { name: 'Laske arvio' }).click();
+
+  expect(
+    await page
+      .getByText(
+        'Arvon pitää olla 0 tai enemmän: Palveluasumisen vuorokausihinta.',
+      )
+      .isVisible(),
+  ).toBeFalsy();
 });
--- tests/sites/sote/continuous-housing-service-voucher/results.spec.ts
+++ tests/sites/sote/continuous-housing-service-voucher/results.spec.ts
@@ -10,8 +10,8 @@ const TEST_CASES = [
   {
     NAME: 'Low',
     NET_INCOME_PER_MONTH: '0',
-    SERVICE_PROVIDER_PRICE: '180',
-    RESULT: '2175,00',
+    SERVICE_PROVIDER_PRICE: '120',
+    RESULT: '350,00',
   },
   {
     NAME: 'Medium',
@@ -22,8 +22,8 @@ const TEST_CASES = [
   {
     NAME: 'High',
     NET_INCOME_PER_MONTH: '9000',
-    SERVICE_PROVIDER_PRICE: '180',
-    RESULT: '4975,00',
+    SERVICE_PROVIDER_PRICE: '210',
+    RESULT: '5887,50',
   },
 ];
 
