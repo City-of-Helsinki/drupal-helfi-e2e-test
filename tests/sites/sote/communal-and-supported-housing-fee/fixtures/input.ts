4e3a8986fc45e866fc62b5bc1c2ea51d74cfaeaf
Anna Lonka <anna.lonka@druid.fi>
2025-12-05 15:56:38 +0200

    Started with communal and supported housing tests

---
+++ tests/sites/sote/communal-and-supported-housing-fee/fixtures/input.ts
@@ -0,0 +1,130 @@
+import { expect, test, type Page, type Locator } from '@playwright/test';
+
+async function selectRadioOption(group: Locator, option: 'yes' | 'no') {
+  await group.waitFor({ state: 'visible' });
+
+  const labelText = option === 'yes' ? 'Kyllä, laske arvioon.' : 'Ei';
+  const label = group.locator('label', { hasText: labelText });
+
+  await expect(label).toBeVisible();
+  await label.click();
+
+  const input =
+    option === 'yes' ? group.locator('input[type="radio"]').first() : group.locator('input[type="radio"]').last();
+
+  await expect(input).toBeChecked();
+}
+
+async function selectRadioOptionMultiple(group: Locator, option: 'yes_full_service' | 'yes_partial_service' | 'no') {
+  await group.waitFor({ state: 'visible' });
+
+  let labelText = '';
+  if (option === 'yes_full_service') {
+    labelText = 'Kyllä, laske arvioon täysihoito.';
+  } else if (option === 'yes_partial_service') {
+    labelText = 'Kyllä, laske arvioon puolihoito.';
+  } else {
+    labelText = 'Ei';
+  }
+  const label = group.locator('label', { hasText: labelText });
+
+  await expect(label).toBeVisible();
+  await label.click();
+
+  const input =
+    option === 'yes_full_service' ? group.locator('input[type="radio"]').first() : option === 'yes_partial_service' ? group.locator('input[type="radio"]').nth(1) : group.locator('input[type="radio"]').last();
+
+  await expect(input).toBeChecked();
+}
+
+const fillHouseholdSize = (page: Page, value: string) =>
+  test.step('Fill household size', async () => {
+    await page.getByLabel('Talouden koko (henkilöä)').fill(String(value));
+  });
+
+const fillGrossIncomePerMonth = (page: Page, value: string) =>
+  test.step('Fill gross income per month', async () => {
+    await page.getByLabel('Talouden bruttotulot kuukaudessa (euroa)').fill(String(value));
+  });
+
+const fillMonthlyUsage = (page: Page, value: string) =>
+  test.step('Fill monthly usage', async () => {
+    await page.getByLabel('Palvelun tuntimäärä kuukaudessa (tuntia)').fill(String(value));
+  });
+
+const selectSafetyPhone = async (page: Page) =>
+  test.step('Select safety phone option', async () => {
+    const group = page.getByRole('group', {
+      name: 'Laske arvioon turvapuhelin ja turvaranneke',
+    });
+
+    await selectRadioOption(group, 'yes');
+  });
+
+const selectNoSafetyPhone = (page: Page) =>
+  test.step('Select no safety phone option', async () => {
+    const group = page.getByRole('group', {
+      name: 'Laske arvioon turvapuhelin ja turvaranneke',
+    });
+
+    await selectRadioOption(group, 'no');
+  });
+
+const selectGroceryDeliveryService = (page: Page) =>
+  test.step('Select grocery delivery service', async () => {
+    const group = page.getByRole('group', {
+      name: 'Laske arvioon ruokakaupan kotiinkuljetuspalvelu',
+    });
+
+    await selectRadioOption(group, 'yes');
+  });
+
+const selectNoGroceryDeliveryService = (page: Page) =>
+  test.step('Select grocery delivery service', async () => {
+    const group = page.getByRole('group', {
+      name: 'Laske arvioon ruokakaupan kotiinkuljetuspalvelu',
+    });
+
+    await selectRadioOption(group, 'no');
+  });
+
+const selectMealServiceFull = (page: Page) =>
+  test.step('Select meal service', async () => {
+    const group = page.getByRole('group', {
+      name: 'Laske arvioon ateriapalvelu',
+    });
+
+    await selectRadioOptionMultiple(group, 'yes_full_service');
+  });
+
+  const selectMealServicePartial = (page: Page) =>
+    test.step('Select meal service', async () => {
+      const group = page.getByRole('group', {
+        name: 'Laske arvioon ateriapalvelu',
+      });
+
+    await selectRadioOptionMultiple(group, 'yes_partial_service');
+  });
+
+  const selectNoMealService = (page: Page) =>
+    test.step('Select meal service', async () => {
+      const group = page.getByRole('group', {
+        name: 'Laske arvioon ateriapalvelu',
+      });
+
+    await selectRadioOptionMultiple(group, 'no');
+  });
+
+
+export {
+  fillHouseholdSize,
+  fillGrossIncomePerMonth,
+  fillMonthlyUsage,
+  selectSafetyPhone,
+  selectNoSafetyPhone,
+  selectGroceryDeliveryService,
+  selectNoGroceryDeliveryService,
+  selectMealServiceFull,
+  selectMealServicePartial,
+  selectNoMealService,
+};
